241f02901c0442df7f1263740a98b90639b9c1b3
Victoria <vl111@wellesley.edu>
2025-01-15 11:39:20 -0500

    added component and style folder, deleted vite and react logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import './style/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <h1>Is it a Sandwich?</h1>
-    </>
+    </div>
   )
 }
 
